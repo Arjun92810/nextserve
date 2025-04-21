120ee47324c97dd44d0f6c1db80b2da1fc7cb23e
Amey Dhavle <2028adhavle@franklinjc.org>
2025-04-21 02:42:50 -0400

    login error fixed

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,11 +2,11 @@
 
 import dynamic from 'next/dynamic';
 
-// Import LoginForm with no SSR
+// Dynamically import LoginForm with SSR disabled
 const LoginForm = dynamic(() => import('@/components/LoginForm'), {
   ssr: false,
 });
 
 export default function LoginPage() {
   return <LoginForm />;
-} 
+}
